d075f5d832be95b1217d98e6a243c8151d2586d5
Matias Weisz <matiasweisz1@gmail.com>
2024-02-23 12:34:00 -0300

    Aplicando llamadas simples a APIs

--- buscador-local/src/views/Search/index.js
+++ buscador-local/src/views/Search/index.js
@@ -1,14 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import SearchBox from "./components/SearchBox";
 import "./style.css"
-import data from "../../data/users.json"
 import SearchResults from "./components/SearchResults";
 
 export default function Search(){
 
 const [isAtTop, setIsAtTop] = useState(false)
 const [results, setResults] = useState([])
+const [data, setData] = useState([])
 
+useEffect(() => {
+    const getUsers = async () => {
+        const response = await fetch("https://jsonplaceholder.typicode.com/users")
+        const data = await response.json()
+            setData(data)
+    }
+    getUsers().catch(null)
+
+}, [])
+   
 const handleCloseSearch = () => {
     setIsAtTop(false)
     setResults([])
@@ -23,11 +33,10 @@ const handleSearchClick = (searchText) => {
                     value.email.toLowerCase().includes(searchTextMinus) ||
                     value.username.toLowerCase().includes(searchTextMinus)
             );
-
             setResults(filterData)
         }
     }
-
+    
     return (
         <div className={`search ${isAtTop ? "search--top" : "search--center"}`}>
             <SearchBox 
